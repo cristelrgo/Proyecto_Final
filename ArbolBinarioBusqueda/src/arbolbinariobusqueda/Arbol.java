0a0b462ef690725a9c4b847b5817b3654cfb06b6
Cristel Garcia <cristelrociogar@gmail.com>
2015-11-18 20:39:54 -0600

    Metodos de busqueda y recorrido (Preorder, Inorder, Posorder)

--- ArbolBinarioBusqueda/src/arbolbinariobusqueda/Arbol.java
+++ ArbolBinarioBusqueda/src/arbolbinariobusqueda/Arbol.java
@@ -5,29 +5,7 @@ public class Arbol {
     
     
     Nodo raiz=null;
-    
-   /*void Insertar(int pDato){
-       
-       Nodo n = new Nodo(pDato);
-       
-       if(raiz==null){
-           raiz = n;
-       
-       }else{
-       if(raiz.dato == n.dato){
-              
-           }else{
-           
-               if(n.dato < raiz.dato){
-               raiz.izquierda = n;
-                   
-               }else{
-               raiz.derecha = n;
-               }
-           }
-       }  
-   }*/
-    
+   
     void insertar (int pDato){
         Nodo n = new Nodo (pDato);
         
@@ -48,9 +26,78 @@ public class Arbol {
                   padre.derecha = n;
              }else {
                  padre.izquierda = n;
-             }
-                 
+             }        
          }
     }
     
- }
+    public Nodo buscar(int valor){
+        Nodo tem = raiz;
+        Nodo aux = tem;
+        
+        while (aux.dato!=valor){
+            if (valor<aux.dato){
+                aux = aux.izquierda;
+            }else{
+                aux = aux.derecha;
+            }
+            if (aux==null){
+                return null;
+            }
+        }
+        return aux;
+    }
+    
+    public int buscaHijoDer(int valor){
+   
+      return  buscar(valor).derecha.dato;   
+    }
+    
+    public int buscaHijoIzq(int valor){
+        
+        return buscar(valor).izquierda.dato;
+    }
+    
+    
+    public void recorridoPreorden(){
+        imprimePre(raiz);
+    }
+    
+    private void imprimePre(Nodo nodo)
+    {
+        if(nodo == null)
+            return;
+         
+        System.out.print(nodo.dato + " ");     
+        imprimePre(nodo.izquierda);   
+        imprimePre(nodo.derecha);     
+    }
+     
+    public void recorridoInorden()
+    {
+        imprimeIno(raiz);
+    }
+     
+    private void imprimeIno(Nodo nodo)
+    {
+        if(nodo == null)
+            return;
+         
+        imprimeIno(nodo.izquierda);
+        System.out.print(nodo.dato + " ");
+        imprimeIno(nodo.derecha);
+    }
+     
+    public void recorridoPosorden()
+    {
+        imprimePos(raiz);        
+    }
+     
+    private void imprimePos(Nodo nodo){
+        if( nodo == null )
+            return;
+         
+        imprimePos(nodo.izquierda);
+        imprimePos(nodo.derecha);
+        System.out.print(nodo.dato + " ");
+    }
+}
--- ArbolBinarioBusqueda/src/arbolbinariobusqueda/ArbolBinarioBusqueda.java
+++ ArbolBinarioBusqueda/src/arbolbinariobusqueda/ArbolBinarioBusqueda.java
@@ -10,13 +10,28 @@ public class ArbolBinarioBusqueda {
     arbolb.insertar(24);
     arbolb.insertar(4);
     arbolb.insertar(30);
+    arbolb.insertar(21);
     arbolb.insertar(5);
+    
+    
 
-    System.out.println("Raiz derecha: "+arbolb.raiz.derecha.dato);
     System.out.println("Raiz: "+arbolb.raiz.dato);
-    System.out.println("Raiz izquierda: "+arbolb.raiz.izquierda.dato);
-    System.out.println(" razi padre derecha"+arbolb.raiz.derecha.derecha.dato);
-    System.out.println(" razi padre derecha"+arbolb.raiz.izquierda.derecha.dato);
+    System.out.println("Derecha: "+arbolb.raiz.derecha.dato);
+    System.out.println("Izquierda: "+arbolb.raiz.izquierda.dato);
+    System.out.println("hijo derecho de  24: "+arbolb.raiz.derecha.derecha.dato);
+        System.out.println("Hijo izquierdo de 24: "+arbolb.raiz.derecha.izquierda.dato);
+        System.out.println("Hijo derecho de 4:"+ arbolb.raiz.izquierda.derecha.dato);
+    System.out.println("hijo derecho de 20: "+ arbolb.buscaHijoDer(20));
+    System.out.println("hijo derecho de 4: "+arbolb.buscaHijoDer(4));
+        
+    System.out.println("Recorrido Preorder: ");
+    arbolb.recorridoPreorden();
+    
+    System.out.println("\nRecorrido Posorder");
+        arbolb.recorridoPosorden();
+        
+        System.out.println("\nRecorrido Inorder");
+        arbolb.recorridoInorden();
    
     }
     
